fc592b33a834b284f391699ccfbf567c421eed14
xtofhome <cerisara@loria.fr>
2018-08-03 13:53:16 +0200

    plot fft ok

--- plotfft.py
+++ plotfft.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 fig, ax = plt.subplots()
+plt.ion()
+plt.show()
+
 with open("fft.txt","r") as f:
     for l in f:
         x = [float(w) for w in l.split()]
+        print(x[0:10])
         t=np.arange(0,len(x),1)
 
         ax.plot(t,x)
-        plt.show()
+        plt.draw()
+        plt.pause(0.001)
 
-        prin("waiting...")
         plt.waitforbuttonpress(-1)
-
+        plt.cla()
